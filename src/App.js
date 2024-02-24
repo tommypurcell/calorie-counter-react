572c233dd183e561aefa0f0e51a95817faa732f3
tommypurcell <tommypurcelljr@gmail.com>
2024-02-24 18:23:51 +0700

    Deleted unnecessary routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import logo from './logo.svg'
 import './App.css'
-import House from './pages/House'
+import React from 'react'
+import logo from './logo.svg'
+import Stats from './pages/Stats'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import FoodLog from './pages/FoodLog'
-import SignUp from './pages/SignUp'
-import HouseCreate from './pages/HouseCreate'
-import HouseEdit from './pages/HouseEdit'
-import Stats from './pages/Stats'
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import React from 'react'
-import HouseThumbnail from './components/HouseThumbnail'
-import MealPlanGenerator from './pages/MealPlanGenerator'
-import CalorieCounter from './pages/CalorieCounter'
 import LandingPage from './pages/LandingPage'
+import CalorieCounter from './pages/CalorieCounter'
+import MealPlanGenerator from './pages/MealPlanGenerator'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -22,13 +17,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/house/:id" element={<House />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/house-create" element={<HouseCreate />} />
-        <Route path="/house-edit" element={<HouseEdit />} />
         <Route path="/foodlog" element={<FoodLog />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/calorie-counter" element={<CalorieCounter />} />
         <Route path="/stats" element={<Stats />} />
         <Route path="/meal-plan-generator" element={<MealPlanGenerator />} />
