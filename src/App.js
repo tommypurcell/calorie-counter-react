6b763f589d209286906e3215c8717f683079d501
tommypurcell <tommypurcelljr@gmail.com>
2024-03-03 17:28:01 +0700

    Added state and checklogin to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
+// imports
 import './App.css'
 import React from 'react'
-import logo from './logo.svg'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// import pages
 import Stats from './pages/Stats'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Home from './pages/Home'
 import FoodLog from './pages/FoodLog'
 import LandingPage from './pages/LandingPage'
 import CalorieCounter from './pages/CalorieCounter'
 import MealPlanGenerator from './pages/MealPlanGenerator'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// import components
+import Nav from './components/Nav'
+
+// Set up axios configurations
+axios.defaults.withCredentials = true
+const local_url = 'http://localhost:4000'
 
 function App() {
+  // State for managing logged-in status
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  // Function to check if user is logged in
+  const checkLogin = async () => {
+    try {
+      let user = await axios.get(`${local_url}/profile`)
+      if (user.data !== 'Not authorized') {
+        setLoggedIn(true)
+      } else {
+        setLoggedIn(false)
+      }
+    } catch (err) {
+      console.error('Error checking login:', err.message)
+    }
+  }
+
+  // Set state of logged in to display nav button as loggedin or loggedout
+  const handleLogin = () => {
+    setLoggedIn(true)
+  }
+  const handleLogout = () => {
+    setLoggedIn(false)
+  }
+
+  // Effect hook to check login status on mount
+  useEffect(() => {
+    checkLogin()
+  }, [])
+
   return (
     // Router
     <BrowserRouter>
+      {/* Pass loggedIn state and handleLogout function as props to Nav */}
+      <Nav loggedIn={loggedIn} onLogout={handleLogout} />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/foodlog" element={<FoodLog />} />
         <Route path="/signup" element={<SignUp />} />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,43 +1,31 @@
+/* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import React, { Component } from 'react'
 axios.defaults.withCredentials = true
 
 let render_url = 'https://calorie-counter-api-portalversion.onrender.com'
 let local_url = 'http://localhost:4000'
 
-export default function Nav() {
+export default function Nav(props) {
+  console.log(props.loggedIn)
   const navigate = useNavigate()
-
-  const [loggedIn, setLoggedIn] = useState(false)
-
-  // check if user is logged in
-  const checkLogin = async () => {
-    let user = await axios.get(`${render_url}/profile`)
-    if (user.data !== 'Not authorized') {
-      setLoggedIn(true)
-    } else {
-      setLoggedIn(false)
-    }
-  }
 
   const requestLogout = async (e) => {
     e.preventDefault()
-    let userToLogout = await axios.get(`${render_url}/logout`)
+    let userToLogout = await axios.get(`${local_url}/logout`)
     console.log(userToLogout.data)
+    props.onLogout()
     navigate('/login')
   }
-
-  useEffect(() => {
-    checkLogin()
-  }, [])
 
   return (
     <>
       {/* nav bar */}
       <nav className="logo-bar container">
-        <div className="d-flex flex-column flex-md-row nav-div">
+        <div className="d-flex flex-column flex-md-row g-4 nav-div">
           <Link to="/calorie-counter" className="m-2 w-100 text-center nav-button">
             Calorie Counter
           </Link>
@@ -47,25 +35,22 @@ export default function Nav() {
           <Link to="/meal-plan-generator" className="m-2 w-100 text-center nav-button">
             Meal Plan Generator
           </Link>
-          <Link to="/stats" className="m-2 w-100 text-center nav-button">
+          <Link to="/stats" className="m-2 w-100 text-center nav-button position-relative">
             Stats
           </Link>
-          <Link to="/profile" className='m-2 w-100 text-center nav-button '>Profile</Link>
-          {loggedIn ? (
-            <a
-              onClick={(e) => requestLogout(e)}
-              type="submit"
-              className="btn btn-outline-secondary m-2 w-100 text-center nav-button"
-              style={{ height: 44, marginLeft: 5 }}
-            >
+          <Link to="/profile" className="m-2 w-100 text-center nav-button d-flex flex-row gap-2 position-relative align-items-center">
+            <img src="https://randomuser.me/api/portraits/men/33.jpg" alt="" className="rounded-circle h-100 m-0 w-auto" />
+            <span>Profile</span>
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded bg-danger">
+              <span>1</span>
+            </span>
+          </Link>
+          {props.loggedIn ? (
+            <a onClick={(e) => requestLogout(e)} type="submit" className="btn btn-outline-secondary m-2 w-100 text-center nav-button" style={{ height: 44, marginLeft: 5 }}>
               Logout
             </a>
           ) : (
-            <a
-              href="/login"
-              className="btn btn-outline-secondary m-2 w-100 text-center nav-button"
-              style={{ height: 44, marginLeft: 5 }}
-            >
+            <a href="/login" className="btn btn-outline-secondary m-2 w-100 text-center nav-button" style={{ height: 44, marginLeft: 5 }}>
               Login
             </a>
           )}
