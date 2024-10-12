a7dc768692666d1a32485930e9b5396869693e51
tommypurcell <tommypurcelljr@gmail.com>
2024-10-12 12:16:03 -0700

    styling login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -53,7 +53,9 @@ export default function Login(props) {
       <div className="grid grid-cols-2 h-full">
         <div
           style={{
-            backgroundColor: '#ddffaa',
+            backgroundColor: `#DAFFFD`,
+
+            // backgroundColor: '#ddffaa',
             backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='120' height='120' viewBox='0 0 120 120'%3E%3Cpolygon fill='%23AE9' points='120 120 60 120 90 90 120 60 120 0 120 0 60 60 0 0 0 60 30 90 60 120 120 120 '/%3E%3C/svg%3E")`
           }}
           className="h-full"
