c21fd820744511f9a4527c9a586da018ebd8867f
tommypurcell <tommypurcelljr@gmail.com>
2025-10-26 13:30:14 -0700

    bug fix check all on profile

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -20,7 +20,7 @@ export default function AuthCallback() {
         const user = session.user
         console.log('User detected:', user.email)
 
-        const { data: profile } = await supabase.from('profiles').select('name, avatar, calorieGoal, bmi, bmr, gender, height_cm, weight_kg, activity_level').eq('id', user.id).single()
+        const { data: profile } = await supabase.from('profiles').select('*').eq('id', user.id)
 
         if (profile) {
           if (profile.name) localStorage.setItem('name', profile.name)
@@ -30,7 +30,7 @@ export default function AuthCallback() {
 
         console.log('Profile:', profile)
 
-        if (!profile || !profile.bmi || !profile.bmr || !profile.gender || !profile.height_cm || !profile.weight_kg || !profile.activity_level) {
+        if (!profile.bmi || !profile.bmr || !profile.gender || !profile.height_cm || !profile.weight_kg || !profile.activity_level) {
           navigate('/on-boarding')
         } else {
           navigate('/', { replace: true })
